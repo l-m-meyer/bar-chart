64d9aa53d17dd78d86e27e9e6635bc6da70a344c
Lisa Meyer <lisamermey@gmail.com>
2022-02-24 16:24:05 -0500

    draft functions

---
+++ barchart.js
@@ -0,0 +1,33 @@
+/* Signature of the function to be used by HTML to render bar chart.
+* @param {array} data -- an array of numbers
+* @param {object} options -- object that contains the stylistic elements of chart (e.g. width, height)
+* @param {string} element -- DOM/jQuery element the chart will be rendered into
+*/
+
+// signature of the function
+function drawBarChart(data, options, element) {
+
+}
+
+// create title of bar chart
+function createTitle(title) {
+  //create the element
+  //font size
+  //font colour
+}
+
+// set bar sizes
+function barSizes(width, height) {
+
+}
+
+// stylize bar chart
+function styleBar(bar, label, spacing, axes) {
+
+}
+
+// X-axis labels for data
+function xAxis() {}
+
+// Y-axis labels for data
+function yAxis() {}
